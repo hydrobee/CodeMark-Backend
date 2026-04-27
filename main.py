4854c279caaccb922c4a33c8ee2e99aaebbf5ceb
steve_ojt <steve_ojt@sarawakskills.edu.my>
2026-04-27 20:31:21 +0800

    fix: add Vercel URL to CORS

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 # Add CORS middleware for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],  # Update this with your frontend URL in production
+    allow_origins=["http://localhost:4200", "https://code-mark-frontend-vcwp.vercel.app"],  # Update this with your frontend URL in production
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["*"],
